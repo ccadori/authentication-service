f9a7fd0fdd0b7ddcacda494278902403684b51f3
ccadori <carloscadori.dev@gmail.com>
2020-05-11 22:24:07 -0300

    adding login tests

--- tests/model/login.test.js
+++ tests/model/login.test.js
@@ -12,11 +12,36 @@ afterAll(async () => {
 });
 
 describe("Login model", () => {
-  it ("Should reject invalid parameters user", async () => {
-    const invalidEmailResult = await model.create(null, "123123", connection);
-    expect(invalidEmailResult.error).toBeDefined();
+  describe("Create", () => {
+    it ("Should reject invalid parameters user", async () => {
+      const invalidEmailResult = await model.create(null, "123123", connection);
+      expect(invalidEmailResult.error).toBeDefined();
+  
+      const invalidPasswordResult = await model.create("test@test.com", null, connection);
+      expect(invalidPasswordResult.error).toBeDefined();
+    });
+  
+    it ("Should reject unmatch user password and email", async () => {
+      const result = await model.create('test@test.com', 'test321', connection);
+      expect(result.error).toBeDefined();
+    });
+  
+    it ("Should return a token", async () => {
+      const result = await model.create('test@test.com', 'test123', connection);
+      expect(result.data).toBeDefined();
+    });
+  });
 
-    const invalidPasswordResult = await model.create("test@test.com", null, connection);
-    expect(invalidPasswordResult.error).toBeDefined();
+  describe("Verifiy", () => {
+    it ("Should reject an invalid token", async () => {
+      const result = await model.verifiy("test", connection);
+      expect(result.code).toBeFalsy();
+    });
+
+    it ("Should accept an valid token", async () => {
+      const loginResult = await model.create('test@test.com', 'test123', connection);
+      const result = await model.verifiy(loginResult.data, connection);
+      expect(result.data).toBeTruthy();
+    });
   });
 });
